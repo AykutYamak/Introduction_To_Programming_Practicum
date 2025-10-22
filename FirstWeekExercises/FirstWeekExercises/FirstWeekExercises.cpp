180bc8c46274b2cd92db367895d7614ac9f3ae29
AykutYamak <159625456+AykutYamak@users.noreply.github.com>
2025-10-22 15:59:33 +0300

    Add files via upload

---
+++ FirstWeekExercises/FirstWeekExercises/FirstWeekExercises.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+int main() {
+	//ex 11
+	/*int a, b, c, d;
+	std::cout << "First interval: ";
+	std::cin >> a >> b;
+	std::cout << "Second interval: ";
+	std::cin >> c >> d;
+	std::cout << (a > c || b > d);*/
+	
+	////ex 12
+	//int a, b, c, d, e;
+	//std::cin >> a >> b >> c >> d >> e;
+	//std::cout << (a + 1 == b && b + a == c && c + b == d && d + c == e);
+	
+	//ex 13
+	/*int a;
+	std::cin >> a;
+	int sum = a * (a + 1) / 2;
+	std::cout << (sum);*/
+	
+	//ex 14
+	int n;
+	int sum = 0;
+	
+}
