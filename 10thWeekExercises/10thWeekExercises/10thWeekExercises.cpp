4d0a1da69860fc55971cbcd9175582ec1de0cc7d
AykutYamak <aykut2006yamak@gmail.com>
2025-12-12 01:55:29 +0200

    added solutions

---
+++ 10thWeekExercises/10thWeekExercises/10thWeekExercises.cpp
@@ -0,0 +1,197 @@
+﻿#include <iostream>
+constexpr int M = 3;
+constexpr int N = 3;
+
+int x = -1;
+int y = -1;
+
+int matrix[M][N];
+int matrix2[M][N];
+
+
+void print(int matrix[][N], unsigned rows, unsigned cols) {
+    for (unsigned i = 0; i < rows; i++)
+    {
+        for (unsigned j = 0; j < cols; j++)
+        {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+void InsertChar(char matrix[][N], unsigned rows, unsigned cols) {
+    for (unsigned i = 0; i < rows; i++)
+    {
+        for (unsigned j = 0; j < cols; j++)
+        {
+            std::cin >> matrix[i][j];
+        }
+    }
+    std::cout << std::endl;
+}
+void Insert(int matrix[][N], unsigned rows, unsigned cols) {
+    for (unsigned i = 0; i < rows; i++)
+    {
+        for (unsigned j = 0; j < cols; j++)
+        {
+            std::cin >> matrix[i][j];
+        }
+    }
+    std::cout << std::endl;
+}
+int findSmallestElement(int matrix[][N], unsigned rows, unsigned cols) {
+    int min = INT_MAX;
+    for (unsigned i = 0; i < rows; i++)
+    {
+        for (unsigned j = 0; j < cols; j++)
+        {
+            if (min > matrix[i][j])
+            {
+                min = matrix[i][j];
+            }
+        }
+    }
+    return min;
+}
+void printGlavenDiagonalIposleVtorostepenniq(int matrix[N][N]) {
+    for (unsigned i = 0; i < N; i++)
+    {
+        std::cout << matrix[i][i] << " ";
+    }
+    std::cout << std::endl;
+    for (unsigned i = 0; i < N; i++)
+    {
+        std::cout << matrix[i][N - i - 1] << " ";
+    }
+}
+void printZigZag(int matrix[][N],int rows, int cols) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (i%2!=0)
+            {
+                std::cout << matrix[i][N - j - 1] << " ";
+            }
+            else
+            {
+                std::cout << matrix[i][j] << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+void findCoordinates(int matrix[][N], int rows, int cols, int n, int &x, int &y) {
+    for (unsigned i = 0; i < rows; i++)
+    {
+        for (unsigned j = 0; j < cols; j++)
+        {
+            if (matrix[i][j]==n)
+            {
+                x = i;
+                y = j;
+            }
+        }
+    }
+    if (x == -1 && y == -1)
+    {
+        std::cout << x << "," << y;
+    }
+    else
+    {
+        std::cout << x << "," << y;
+    }   
+}
+void sum(int matrix[M][N], int matrix2[M][N], int rows, int cols) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i][j] = matrix[i][j] + matrix2[i][j];
+        }
+    }
+}
+void sumWithoutTouchingTheMatrix(int matrix[M][N], int matrix2[M][N],int rows, int cols) {
+    int matrixSum[M][N];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrixSum[i][j] = matrix[i][j] + matrix2[i][j];
+        }
+    }
+    print(matrixSum, M, N);
+}
+bool isUpper(char a) {
+    if (a>='A' && a<='Z')
+    {
+        return true;
+    }
+    return false;
+}
+void findStringWithTheMostUppercaseLetters(char matrix[M][N],int rows, int cols) {
+    char arr[N];
+    int row = 0;
+    int count = 0;
+    int buf = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (isUpper(matrix[i][j]))
+            {
+                count++;
+            }
+        }
+        if (buf < count)
+        {
+            buf = count;
+            row = i;
+        }   
+        count = 0;
+    }
+    for (int i = 0; i < cols; i++)
+    {
+        std::cout << matrix[row][i] << " ";
+    }
+
+}
+int main()
+{
+
+    //01
+   /* 
+    Insert(matrix, M, N);
+    print(matrix, M, N);*/
+
+    //02
+    /*
+    Insert(matrix, M, N);
+    std::cout << findSmallestElement(matrix, M, N);*/
+    
+    //03
+    /*Insert(matrix, M, N);
+    printGlavenDiagonalIposleVtorostepenniq(matrix);*/
+    
+    //04
+    /*Insert(matrix, M, N);
+    printZigZag(matrix,M,N);*/
+    
+    //05
+    /*Insert(matrix, M, N);
+    findCoordinates(matrix, M, N, 6, x, y);*/
+    
+    //06
+    //Insert(matrix, M, N);
+    //Insert(matrix2, M, N);
+    /*sum(matrix, matrix2, M, N);
+    print(matrix,M,N);*/
+    //sumWithoutTouchingTheMatrix(matrix, matrix2, M, N);
+    
+    //07
+    //char charMatrix[M][N];
+    //InsertChar(charMatrix, M, N);
+    //findStringWithTheMostUppercaseLetters(charMatrix,M,N);
+
+    //08
+}
--- FifthWeekExercises/FifthWeekExercises/FifthWeekExercises.cpp
+++ FifthWeekExercises/FifthWeekExercises/FifthWeekExercises.cpp
@@ -22,6 +22,17 @@ void findSmallestAndBiggestNumber(const int arr[], int size) {
     }
     std::cout << "Smallest number is: " << min << ' '<< std::endl << "Biggest number is : " << max;
 }
+int findSmallestNumber(const int arr[], int size) {
+    int min = arr[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
 int removeElementByIndex(int arr[], int size, int index) 
 {
     for (int i = index; i < size-1; i++)
@@ -31,7 +42,7 @@ int removeElementByIndex(int arr[], int size, int index)
     size--;
     return size;
 }
-bool isPalindrom(int arr[], int size) {
+bool isPalindrom(const int arr[], int size) {
     bool isPalindrom = true;
     for (int i = 1; i < size; i++)
     {
@@ -50,37 +61,274 @@ int GCD(int a, int b) {
     int min = a < b ? a : b;
     for (int i = min; i > 0; i--)
     {
-        if ((a%min == 0) && (b % min == 0))
+        if ((a % i == 0) && (b % i== 0))
         {
             return i;
         }
     }
 }
 int FindGCDofElementsInArray(int arr[], int size) {
-    int gcdoftwonumbers = arr[0];
+    int result = 0;
+    result = GCD(arr[0], arr[1]);
+    bool check = false;
+    for (int i = 1; i < size-1; i++)
+    {
+        if (result == GCD(arr[i],arr[i+1]))
+        {
+            check = true;
+        }
+        else
+        {
+            check = false;
+        }
+    }
+    if (check == true)
+    {
+        return result;
+    }
+    return 0;
+}
+bool isConcatOfTwoPalindroms(int arr[], int SIZE) {
+    int firstElement = arr[0];
+    for (int i = 1; i < SIZE; i++)
+    {
+        if (arr[0] == arr[i])
+        {
+            if (arr[i+1] == arr[SIZE - 1])
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+bool isSortedAscending(int arr[], int size) {
+    bool check = false;
+    for (int i = 0; i < size-2; i++)
+    {
+        if (arr[i]<=arr[i+1])
+        {
+            check = true;
+        }
+        else
+        {
+            check = false;
+            break;
+        }
+    }
+    return check;
+}
+int rfind(int arr[], int size, int value) {
+    for (int i = size-1; i >= 0; i--)
+    {
+        if (arr[i] == value)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int find(int arr[], int size, int value)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == value)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+bool BinarySearch(int arr[], int size, int value) {
+    bool check = false;
+    int left = 0, right = size - 1;
+    while (left<=right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value)
+        {
+            check = true;
+            return check;
+        }
+        else if (arr[mid] < value)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return check;
+}
+int longestNonDecreasingSubsequence(int arr[], int size) {
+    int subsequence = 1;
+    int longestSubsequence = 0;
     for (int i = 1; i < size; i++)
     {
-        GCD(gcdoftwonumbers,arr[i]);
+        
+        if (arr[i-1] <= arr[i])
+        {
+            subsequence++;
+            if (longestSubsequence <= subsequence)
+            {
+                longestSubsequence = subsequence;
+            }
+        }
+        else
+        {
+            if (size - subsequence <= subsequence)
+            {
+                return subsequence;
+            }
+            else
+            {
+                longestSubsequence = subsequence;
+                subsequence = 1;
+            }
+        }
+    }
+    return longestSubsequence;
+}
+void section(int arr1[], int arr2[], int size1, int size2, int newarr[])
+{   
+    int k = 0;
+    for (int i = 0; i < size1; i++)
+    {
+        bool found = false;
+        for (int j = 0; j < size2; j++)
+        {
+            if (arr1[i] == arr2[j])
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            continue;
+        }
+        bool exists = false;
+        for (int t = 0; t < k; t++)
+        {
+            if (newarr[t] == arr1[i])
+            {
+                exists = true;
+                break;
+            }
+        }
+        if (!exists)
+        {
+            newarr[k] = arr1[i];
+            k++;
+        }
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        std::cout << newarr[i] << " ";
     }
 }
+void merge(int arr1[], int arr2[], int size1, int size2, int result[])
+{
+    int i = 0, j = 0, k = 0;
+
+    while (i < size1 && j < size2)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            result[k] = arr1[i];
+            k++; 
+            i++;
+        }    
+        else
+        {
+            result[k] = arr2[j];
+            j++;
+            k++;
+        }    
+    }
+   
+    while (i < size1) 
+    {
+        result[k++] = arr1[i++];
+    }
+    while (j < size2)
+    {
+        result[k++] = arr2[j++];
+    }
+    for (int i = 0; i < k; i++)
+    {
+        std::cout << result[i] << " ";
+    }
+}
+//void hasUnimodalSubarray(int arr[], int start, int end) {
+//    for (int i = start; i < end-1; i++)
+//    {
+//
+//    }
+//}
 int main()
 {
     // 1
     constexpr int SIZE = 5;
     int arr[SIZE];
     addElementsToArray(arr, SIZE);
+    
+    //02
     /*for (int i  = 0; i < SIZE; i++)
     {
         std::cout << arr[i];
     }*/
     /*findSmallestAndBiggestNumber(arr, SIZE);*/
- /*   int index;
-    std::cin >> index;
-    int newSize = removeElementByIndex(arr, SIZE, index);
-    for (int i = 0; i < newSize; i++)
-    {
-        std::cout << arr[i];
-    }*/
+    
+    //03
+    //int index;
+    //std::cin >> index;
+    //int newSize = removeElementByIndex(arr, SIZE, index);
+    //for (int i = 0; i < newSize; i++)
+    //{
+    //    std::cout << arr[i];
+    //}
+
+    //04
     //std::cout << (isPalindrom(arr, SIZE) ? "true" : "false");
     
+    //05
+    //std::cout << "GCD of the elements is: " << (FindGCDofElementsInArray(arr, SIZE));
+
+    //06
+    //std::cout << (isConcatOfTwoPalindroms(arr, SIZE) ? "TRUE" : "FALSE");
+    
+    //07
+    //std::cout << ((isSortedAscending(arr, SIZE) ? "TRUE" : "FALSE"));
+
+    //08
+    //std::cout << find(arr, SIZE, 2) << std::endl;
+    //std::cout << rfind(arr, SIZE, 5);
+    
+    //09
+    //std::cout << "Enter target: ";
+    //int number;
+    //std::cin >> number;
+    //std::cout << ((BinarySearch(arr, SIZE, number) ? "true" : "false"));
+    
+    //10
+    //std::cout << longestNonDecreasingSubsequence(arr, SIZE);
+
+    //11
+    /*std::cout << "Enter the second arrays elements: ";
+    constexpr int SIZE2 = 3;
+    int arr2[SIZE2];
+    addElementsToArray(arr2, SIZE2);
+    int newArr[(SIZE < SIZE2 ? SIZE2 : SIZE)];
+    section(arr, arr2, SIZE, SIZE2, newArr);*/
+
+    //12
+    /*constexpr int SIZE2 = 3;
+    int arr2[SIZE2];
+    addElementsToArray(arr2, SIZE2);
+    int mergedArr[SIZE + SIZE2];
+    merge(arr, arr2, SIZE, SIZE2, mergedArr); */
+
 }
