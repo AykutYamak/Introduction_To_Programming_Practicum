8bcf3159791cf9b648b40de9be6cc8dfdc0bdb26
AykutYamak <aykut2006yamak@gmail.com>
2025-12-22 19:22:01 +0200

    added solutions

---
+++ 8thWeekExercises/8thWeekExercises/8thWeekExercises.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+int twoPowered(int k) {
+	if (k < 0 || k > 31)
+	{
+		return -1;
+	}
+	
+	return 1 << k;
+}
+
+int returnBitOnPosition(int n, int m) {
+	return (n >> m)&1;
+}
+
+int getNumberAfterChangedBitOnPosition(int n, int position) {
+	return n ^ (1<<position);
+}
+
+int returnNumber(int x, int m, int n) {
+	return (x >> (m-n+1)) & ((1 << n) - 1);
+}
+
+int encode(int number, int key) {
+	return number ^ key;
+}
+int decode(int code, int key){
+	return code ^ key;
+}
+
+void swap(int* a, int* b)
+{
+	*a = *a ^ *b;
+	*b = *a ^ *b;
+	*a = *a ^ *b;
+}
+
+int main()
+{
+    //01
+	/*int k = 3;
+	std::cout << twoPowered(k);*/
+	
+	//02
+	/*int n = 9;
+	int m = 2;
+	std::cout << returnBitOnPosition(n, m);*/
+	
+	//03
+	/*int n = 5;
+	int position = 0;
+	std::cout << getNumberAfterChangedBitOnPosition(n, position);*/
+	
+	//04
+	int x = 16;
+	int m = 4;
+	int n = 3;
+	std::cout << returnNumber(x, m, n);
+}		
--- 9thWeekExercises/9thWeekExercises/9thWeekExercises.cpp
+++ 9thWeekExercises/9thWeekExercises/9thWeekExercises.cpp
@@ -1,5 +1,13 @@
 ﻿#include <iostream>
 #include <stdbool.h>
+int strLen(char* arr) {
+	int count = 0;
+	for (int i = 0; arr[i]!='\0'; i++)
+	{
+		count++;
+	}
+	return count;
+}
 void LetterToLower(char& letter) {
 	if (letter >= 'A' && letter <= 'Z')
 	{
@@ -12,17 +20,17 @@ void toLower(char* arr) {
 		LetterToLower(arr[i]);
 	}
 }
-bool checkIfPangrama(char* arr) {
+bool isPangrama(char* arr) {
 	for (int i = 0; arr[i]!='\0'; i++)
 	{
-		LetterToLower(arr[i]);		
+		LetterToLower(arr[i]);
 	}
 	bool checked[26] = { false };
 	for (int i = 0; arr[i]!='\0'; i++)
 	{
 		checked[arr[i] - 'a'] = true;	
 	}
-	for (int i = 0;i <26 ; i++)
+	for (int i = 0;i < 26 ; i++)
 	{
 		if (checked[i]!=true)
 		{
@@ -32,15 +40,154 @@ bool checkIfPangrama(char* arr) {
 	return true;
 	
 }
+bool isAnagrama(char* arr, char* arr2) {
+	if (strlen(arr) != strlen(arr2)) 
+	{
+		return false;
+	}
+	int count = 0, lettersCount = 0;
+	char* first = arr;
+	char* second = arr2;
+	int size = strLen(arr);
+	while(*first!='\0')
+	{
+		while(*second!='\0')
+		{
+			if (*second == *first)
+			{
+				count++;
+			}
+			second++;
+		}
+		lettersCount++;
+		count = 0;
+		second = arr2;
+		first++;
+	}
+	if (lettersCount == size)
+	{
+		return true;
+	}
+	return false;
+}
+bool isPalindrome(char* arr) {
+	int size = strLen(arr);
+	char* left = arr;
+	char* right = arr + size-1;
+	while (left<right)
+	{
+		if (*left!=*right)
+		{
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
+}
+bool isPalindromeHelperFunction(char* left, char* right) {
+	while (left < right) {
+		if (*left != *right) {
+			return 0;
+		}
+		left++;
+		right--;
+	}
+	return 1; 
+}
 
+int checkForSubArrayPalindrom(char* arr) {
+	int maxLen = 0;
+	
+	for (char* start = arr; *start != '\0'; start++) {
+		for (char* end = start; *end != '\0'; end++) {
+			if (isPalindromeHelperFunction(start, end)) {
+				int len = end - start + 1;
+				if (len > maxLen) {
+					maxLen = len;
+				}
+			}
+		}
+	}
+	return maxLen;
+}
+
+bool areAllLettersUnique(char* arr) {
+	toLower(arr);
+	char* start = arr;
+	while (*start!='\0')
+	{
+		char* check = start+1;
+		while(*check!='\0')
+		{
+			if (*start == *check)
+			{
+				return false;
+			}
+			check++;
+		}
+		start++;
+	}
+	return true;
+}
+
+void insert(char* text, char* result,char symbol, int i) {
+	for (int j = 0; j < i; j++)
+	{
+		result[j] = text[j];
+	}
+	result[i] = symbol;
+	int size = strLen(text);
+	for (int j = i; j <= size; j++)
+	{
+		result[j+1] = text[j];
+	}
+
+}
 // 'a' + abs(letter - 'A')
 
 
 int main() {
+	//Code Fragments
+	//1: 10, 13
+	//2: runtime error
+	//3: 3, address, 9
+	//4: error
+	//5: h
+	//6: 7
+	
+	
+	//Practicum Exercises
 	char arr[1024];
 	std::cin.getline(arr, 1024);
-	bool check = checkIfPangrama(arr);
-	std::cout << check;
-
-	return 0;
-}	
+	//01
+	/*toLower(arr);
+	std::cout << arr;*/
+	//02
+	/*bool check = isPangrama(arr);
+	std::cout << (check ? "true" : "false");*/
+	//03
+	//char arr2[1024];
+	//std::cin.getline(arr2, 1024);
+	//std::cout << (isAnagrama(arr, arr2)?"true":"false");
+	//04
+	/*std::cout << (isPalindrome(arr) ? "TRUE" : "FALSE");
+	return 0*/;
+	//05
+	//std::cout << checkForSubArrayPalindrom(arr);
+	//06
+	//std::cout << (areAllLettersUnique(arr)?"TRUE":"FALSE");
+	//07
+	/*char result[1024];
+	char symbol = 'm';
+	int index = 7;
+	insert(arr, result, symbol, index);
+	int length = strLen(arr)+1;
+	for (int i = 0; i < length; i++)
+	{
+		std::cout << result[i];
+	}*/
+	
+	//08
+	
+}
